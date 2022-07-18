59af5ef63e6eab4f859bde4f9649210ab952226f
DavidMunozvi <davidmunozvicode@gmail.com>
2022-07-18 08:44:02 +0200

    manage icon in component

---
+++ src/components/Icons.jsx
@@ -0,0 +1,10 @@
+export {
+  VscFiles as IconMenu,
+  VscColorMode as IconColorMode,
+  VscChevronDown as IcomArrowDown,
+} from "react-icons/vsc";
+
+export {
+  FaGithub as IconGitHub,
+  FaLinkedinIn as IconLinkedIn,
+} from "react-icons/fa";
