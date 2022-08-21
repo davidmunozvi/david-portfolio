7b35749374b36ed326d567ea88c99f73a73478d2
DavidMunozvi <davidmunozvicode@gmail.com>
2022-08-21 23:11:19 +0200

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 const path = require('path')
 
 export default defineConfig({
+  base: '/DavidMunozvi.github.io/',
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src'),
