7c1afa0d5b522ddbe186d74bac53bc64ce7efe37
“davidmunozvi” <davidmunozvicode@gmail.com>
2022-08-23 22:02:50 +0200

    add gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 const path = require('path')
 
 export default defineConfig({
-  base: '/DavidMunozvi.github.io/',
+  base: '/david-portfolio/',
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src'),
